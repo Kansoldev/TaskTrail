a0b262d756375d94d1b2dc7f6a32f8b70dd49f38
Oyinkansola Yahaya <yahayaoyinkansola@gmail.com>
2025-09-16 11:42:53 +0100

    Add some meta data information

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,11 @@
+import type { Metadata } from "next";
 import "../globals.css";
+
+export const metadata: Metadata = {
+  title: "TaskTrail",
+  description:
+    "A to-do list history tracker that tracks your todos and stores your todos in history",
+};
 
 export default function RootLayout({
   children,
