1cf13241ab59f5ea1fe6ce3fe462461d3df772c5
Oyinkansola <yahayaoyinkansola@gmail.com>
2026-03-12 13:47:34 +0100

    feat: Add projects primary font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Baloo_Chettan_2 } from "next/font/google";
 import Header from "@/components/Header";
 import "../globals.css";
 
@@ -8,13 +9,18 @@ export const metadata: Metadata = {
     "A to-do list history tracker that tracks your todos and stores your todos in history",
 };
 
+const balloChettan = Baloo_Chettan_2({
+  subsets: ["latin"],
+  display: "swap",
+});
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={balloChettan.className}>
       <body>
         <Header />
         {children}
